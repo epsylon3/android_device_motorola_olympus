43bd50a2ca97ee3683000323e86f7a12124ff4d2
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-14 00:19:48 +0100

    addons: kernel-only package
    Change-Id: Ic6e48092f3f753b660b22dd44807dd538db4f39d

--- releasetools/addons.sh
+++ releasetools/addons.sh
@@ -10,15 +10,21 @@ echo "addons.sh: $1"
 # remove dummy kernel module (created by kernel repo)
 rm -f $REPACK/ota/system/lib/modules/dummy.ko
 
-cat $DEVICE_TOP/releasetools/updater-addons > $REPACK/ota/META-INF/com/google/android/updater-script
+cat $DEVICE_TOP/releasetools/updater-addons-full > $REPACK/ota/META-INF/com/google/android/updater-script
 cp $DEVICE_OUT/boot.img $REPACK/ota/
 cp $DEVICE_OUT/recovery.img $REPACK/ota/
-
+OUTFILE=$OUT/kernel_and_recovery.zip
 
 if [ "$1" = "recovery" ]; then
 	cat $DEVICE_TOP/releasetools/updater-addons-recovery > $REPACK/ota/META-INF/com/google/android/updater-script
 	rm -rf $REPACK/ota/system
 	rm $REPACK/ota/boot.img
 	OUTFILE=$OUT/recovery.zip
+fi
+
+if [ "$1" = "kernel" ]; then
+        cat $DEVICE_TOP/releasetools/updater-addons-kernel > $REPACK/ota/META-INF/com/google/android/updater-script
+        rm $REPACK/ota/recovery.img
+        OUTFILE=$OUT/kernel.zip
 fi
 
