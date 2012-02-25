79094b85dd4d54032529b4bb3f9f0f778ad5bbbe
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-02-25 08:02:39 +0100

    releasetools: updater-script update
    Change-Id: I114a7690484d7d7ab2cf1c7223cdeb057852946a

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -17,28 +17,37 @@ rm $REPACK/ota/system/etc/init.d/04modules
 rm $REPACK/ota/system/lib/modules/dummy.ko
 
 # add an empty script to prevent logcat errors (moto init.rc)
-touch $REPACK/ota/system/bin/mount_ext3.sh
-chmod +x $REPACK/ota/system/bin/mount_ext3.sh
+#touch $REPACK/ota/system/bin/mount_ext3.sh
+#chmod +x $REPACK/ota/system/bin/mount_ext3.sh
 
 mkdir -p $REPACK/ota/system/etc/terminfo/x
 cp $REPACK/ota/system/etc/terminfo/l/linux $REPACK/ota/system/etc/terminfo/x/xterm
 
 # prebuilt boot, devtree, logo & updater-script
 cp -f $DEVICE_TOP/releasetools/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
-if [ -n "$CYANOGEN_RELEASE" ]; then
+#if [ -n "$CYANOGEN_RELEASE" ]; then
   cat $DEVICE_TOP/releasetools/updater-kernel >> $REPACK/ota/META-INF/com/google/android/updater-script
   cp $DEVICE_TOP/releasetools/custom_backup_full.txt $REPACK/ota/system/etc/custom_backup_list.txt
-else
-  cp $DEVICE_TOP/releasetools/custom_backup_list.txt $REPACK/ota/system/etc/custom_backup_list.txt
-  rm -f $REPACK/ota/boot.img
-fi
+  cp $DEVICE_OUT/boot.img $REPACK/ota/
+  cp $DEVICE_OUT/recovery.img $REPACK/ota/
+#else
+#  cp $DEVICE_TOP/releasetools/custom_backup_list.txt $REPACK/ota/system/etc/custom_backup_list.txt
+#  rm -f $REPACK/ota/boot.img
+#fi
+
+# bootmenu tools
+cp -R -f -p $DEVICE_TOP/bootmenu/* $REPACK/ota/system/bootmenu/
 
 mkdir -p $REPACK/ota/system/bootmenu/2nd-init
-cp $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-init/init
-cp $DEVICE_OUT/root/init.rc $REPACK/ota/system/bootmenu/2nd-init/init.rc
-cp $DEVICE_OUT/root/init.olympus.rc $REPACK/ota/system/bootmenu/2nd-init/init.olympus.rc
-cp $DEVICE_OUT/root/sbin/adbd $REPACK/ota/system/bin/bootmenu/binary/adbd
+cp $DEVICE_OUT/root/init.rc $REPACK/ota/system/bootmenu/2nd-init/
+cp $DEVICE_OUT/root/init.olympus.rc $REPACK/ota/system/bootmenu/2nd-init/
+cp $DEVICE_OUT/root/ueventd.rc $REPACK/ota/system/bootmenu/2nd-init/
+cp $DEVICE_OUT/root/ueventd.olympus.rc $REPACK/ota/system/bootmenu/2nd-init/
+
+mkdir -p $REPACK/ota/system/bootmenu/2nd-boot
+cp $REPACK/ota/system/bootmenu/binary/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-boot
+cp $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-boot/
 
 # use the static busybox in bootmenu, this helps a lot on incomplete roms and ics
-cp $DEVICE_OUT/utilities/busybox $REPACK/ota/system/bootmenu/binary/busybox
-
+cp $DEVICE_OUT/utilities/logwrapper $REPACK/ota/system/bootmenu/binary/logwrapper.bin
+cp $REPACK/ota/system/bootmenu/binary/logwrapper.bin $REPACK/ota/system/bin/
