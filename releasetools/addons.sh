4ac117557e3f5b8933bd9fe759b91864cdb45a08
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-04-17 16:16:56 +0200

    addons: bootmenu
    Change-Id: Ib79d9ddf34620f4a62e26bcf702727378816a1b0

--- releasetools/addons.sh
+++ releasetools/addons.sh
@@ -9,7 +9,8 @@
 echo "addons.sh: $1"
 
 if [ -z "$1" ]; then
-    echo "addons: missing addon type"
+	echo "addons.sh: error ! no target specified"
+	exit 1
 fi
 
 # remove dummy kernel module (created by kernel repo)
@@ -26,5 +27,36 @@ if [ "$1" = "kernel" ]; then
         cat $DEVICE_TOP/releasetools/updater-addons-kernel > $REPACK/ota/META-INF/com/google/android/updater-script
         cp $DEVICE_OUT/boot.img $REPACK/ota/
         OUTFILE=$OUT/kernel.zip
+fi
+
+if [ "$1" = "bootmenu" ]; then
+
+	cd $REPACK/ota
+	unzip -q $OTAPACKAGE "system/bootmenu/*" "system/bin/*"
+
+	cat $DEVICE_TOP/releasetools/updater-addons-bootmenu > $REPACK/ota/META-INF/com/google/android/updater-script
+
+	#atrix bootmenu require a custom kernel to allow the framebuffer
+	cp $DEVICE_OUT/boot.img $REPACK/ota/
+	cat $DEVICE_TOP/releasetools/updater-addons-kernel >> $REPACK/ota/META-INF/com/google/android/updater-script
+
+	mv $REPACK/ota/system/bin/bootmenu $REPACK/ota/system/bootmenu/binary/bootmenu
+	cp $DEVICE_OUT/utilities/busybox $REPACK/ota/system/bootmenu/binary/
+	rm -r -f $REPACK/ota/system/bin
+	mkdir -p $REPACK/ota/system/bin
+	mv $REPACK/ota/system/bootmenu/binary/bootmenu $REPACK/ota/system/bin/bootmenu
+	# cp $DEVICE_OUT/system/bin/toolbox $REPACK/ota/system/bin/
+
+	mkdir -p $REPACK/ota/system/bootmenu/2nd-init
+	cp $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-init/
+	cp $DEVICE_OUT/root/*.rc $REPACK/ota/system/bootmenu/2nd-init/
+	cp $DEVICE_TOP/init $REPACK/ota/system/bootmenu/2nd-init/
+
+	mkdir -p $REPACK/ota/system/bootmenu/2nd-boot
+	cp $REPACK/ota/system/bootmenu/binary/2nd-init $REPACK/ota/system/bootmenu/binary/2nd-boot
+	cp $DEVICE_OUT/root/init $REPACK/ota/system/bootmenu/2nd-boot/
+	cp $DEVICE_OUT/root/*.rc $REPACK/ota/system/bootmenu/2nd-boot/
+
+	OUTFILE=$OUT/bootmenu_stock.zip
 fi
 
